8cff54c2b92f7472dfbc4a8cf7787abf4b2b17e2
Alexandra <alexandraneff@live.com>
2023-03-06 21:25:38 -0600

    added get requests for routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,24 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/top_games' do
+    top_games = TopGame.all 
+    top_games.to_json
   end
+
+  get '/anticipated_games' do 
+    anticipated_games = AntcipatedGame.all
+    antcipated_games.to_json
+  end 
+
+  get '/reviews' do
+    reviews = Review.all 
+    reviews.to_json 
+  end 
+
+  get '/users' do 
+    users = User.all 
+    users.to_json
+  end 
 
 end
